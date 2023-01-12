9fd72d602055ec5cd1c19f45856d7e732ffbc925
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-11 16:59:40 -0800

    add README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,37 @@
 //User Interface Logic
 
 function hideResultsandError() {
-  document.getElementById("error-message").setAttribute("class", "hidden")
+    document.getElementById("error-message").setAttribute("class", "hidden")
 
-  document.getElementById("georgeClooney").setAttribute("class", "hidden");
-  document.getElementById("mariahCarey").setAttribute("class", "hidden");
-  document.getElementById("edSheeran").setAttribute("class", "hidden");
-  document.getElementById("drake").setAttribute("class", "hidden");
-  document.getElementById("michaelJordan").setAttribute("class", "hidden");
-  document.getElementById("donaldGlover").setAttribute("class", "hidden");
+    document.getElementById("georgeClooney").setAttribute("class", "hidden");
+    document.getElementById("mariahCarey").setAttribute("class", "hidden");
+    document.getElementById("edSheeran").setAttribute("class", "hidden");
+    document.getElementById("drake").setAttribute("class", "hidden");
+    document.getElementById("michaelJordan").setAttribute("class", "hidden");
+    document.getElementById("donaldGlover").setAttribute("class", "hidden");
 }
 
 // Onload function
-window.onload = function() {
-  document.querySelector("form").onsubmit = function(event) {
-    event.preventDefault();
-    hideResultsandError();
-    const age = parseInt(document.querySelector("input#age").value);
-    const gender = document.querySelector("input#gender").value;
-  if (age && gender) {
-    if (age <= 49 && gender === "male") {
-      document.getElementById("georgeClooney").removeAttribute("class");
-    } else if (age >=50 && gender === "male") {
-      document.getElementById("mariahCarey").removeAttribute("class");
-    } else if (age <= 49 && gender === "female") {
-      document.getElementById("edSheeran").removeAttribute("class");
-    } else if (age >= 50 && gender === "female") {
-      document.getElementById("drake").removeAttribute("class");
-    } else if (age <= 49 && gender === "nonbinary") {
-      document.getElementById("michaelJordan").removeAttribute("class");
-    } else if (age >= 50 && gender === "nonbinary") {
-      document.getElementById("donaldGlover").removeAttribute("class");
+window.onload = function () {
+    document.querySelector("form").onsubmit = function (event) {
+        event.preventDefault();
+        hideResultsandError();
+        const age = parseInt(document.querySelector("input#age").value);
+        const gender = document.querySelector("input#gender").value;
+        if (age && gender) {
+            if (age <= 49 && gender === "male") {
+                document.getElementById("georgeClooney").removeAttribute("class");
+            } else if (age >= 50 && gender === "male") {
+                document.getElementById("mariahCarey").removeAttribute("class");
+            } else if (age <= 49 && gender === "female") {
+                document.getElementById("edSheeran").removeAttribute("class");
+            } else if (age >= 50 && gender === "female") {
+                document.getElementById("drake").removeAttribute("class");
+            } else if (age <= 49 && gender === "nonbinary") {
+                document.getElementById("michaelJordan").removeAttribute("class");
+            } else if (age >= 50 && gender === "nonbinary") {
+                document.getElementById("donaldGlover").removeAttribute("class");
+            };
+        };
     };
-  };
-  };
 };
